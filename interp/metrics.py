2499d818b4fde6841ab79d207e056fbae2bad23e
Ananya Raval <ananya.raval@gmail.com>
2025-01-29 16:12:00 -0500

    Add interp folder; Add metrics and visualize files with empty functions

---
+++ interp/metrics.py
@@ -0,0 +1,31 @@
+# extracted from: https://github.com/kherud/neural-additive-models-pt/tree/master
+
+class PrototypicalLoss():
+    pass
+
+def prototypical_loss():
+    pass
+
+def euclidean_dist(x, y):
+    pass
+
+def feature_loss():
+    pass
+
+def penalized_cross_entropy():
+    pass
+
+def cos_sim_to_prob():
+    pass
+
+def log_prob_to_prob(log_prob):
+    pass
+
+def prob_to_log_prob(prob):
+    pass
+
+def calculate_auroc():
+    pass
+
+def get_roc_auc_score():
+    pass
---
+++ interp/vizualize.py
@@ -0,0 +1,5 @@
+def visualize_trainable_attention():
+    pass
+
+def prototype_heatmap():
+    pass
