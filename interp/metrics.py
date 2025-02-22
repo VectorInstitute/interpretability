bb83b50b5ff73fb9996cf37e18e58abb463c2194
Ananya Raval <ananya.raval@gmail.com>
2025-02-22 02:31:59 -0500

    Add metrics file after resolving conflict

--- interp/metrics.py
+++ interp/metrics.py
@@ -1,7 +1,20 @@
+<<<<<<< Updated upstream
 '''
-<<<<<<< HEAD
 TODO: Add metrics and loss functions
-=======
-TODO: dd metrics and loss functions
->>>>>>> 4579358 (Add interp package and its corresponding pyproject.toml change)
 '''
+=======
+import torch.nn.functional as F
+
+def feature_loss(fnn_out,
+                 lambda_=0.):
+    """
+    """
+    return lambda_ * (fnn_out ** 2).sum() / fnn_out.shape[1]
+
+def penalized_cross_entropy(logits,
+                            truth,
+                            fnn_out,
+                            feature_penalty=0.):
+    """
+    """
+    return F.binary_cross_entropy_with_logits(logits.view(-1), truth.view(-1)) + feature_loss(fnn_out, feature_penalty)
