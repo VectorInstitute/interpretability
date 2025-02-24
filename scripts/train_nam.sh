b80b18ca5d78fc070f1988a52bbb65f5d9b2435e
Ananya Raval <ananya.raval@gmail.com>
2025-02-24 15:30:27 -0500

    Add sbatch script for training NAM

---
+++ scripts/train_nam.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=train-nam
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=5G
+#SBATCH --qos=normal
+#SBATCH --output=job_%x_%j.out
+#SBATCH --error=job_%x_%j.err
+
+python reference_implementations/Intepretable-models/Tabular/NAM-NodeGAM/train_nam.py
