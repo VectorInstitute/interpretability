59bc36989e3388c0ab29bc84f677bd07308bb3ca
Ananya Raval <ananya.raval@gmail.com>
2025-03-11 14:04:21 -0400

    Add imgaug library; Add conditional print statements; Add modularization

--- reference_implementations/Intepretable-models/Imaging/SelfAttention/train_nih.py
+++ reference_implementations/Intepretable-models/Imaging/SelfAttention/train_nih.py
@@ -17,8 +17,9 @@ from typing import Tuple
 from data import Config
 from interp.metrics import get_multiclass_roc_auc_score, get_dist_auc
 from interp.utils import *
-from interp.models import ResNetAttention
-from utils import cleanup, worker_init_fn, save_checkpoint, setup_distributed_training
+from resnetattention import ResNetAttention
+from interp.visualize import visualize_trainable_attention
+from utils import cleanup, worker_init_fn, save_checkpoint, setup_distributed_training, load_checkpoint
 from data import XrayDataset, load_nih_data
 
 def get_data_loaders(cfg: Config,
@@ -39,39 +40,54 @@ def get_data_loaders(cfg: Config,
                                       shuffle=True)
     train_dl = DataLoader(train_ds,
                     sampler=train_sampler,
-                    batch_size=42,
+                    batch_size=cfg.train_params.batch_size,
                     worker_init_fn=init_fn,
                     pin_memory=False,
                     drop_last=True,
                     num_workers=4)
     test_dl = DataLoader(test_ds,
                     sampler=test_sampler,
-                    batch_size=42,
+                    batch_size=cfg.train_params.batch_size,
                     worker_init_fn=init_fn)
     return train_dl, test_dl
 
 def eval_step(model: ResNetAttention,
+              cfg: Config,
               data_dl: DataLoader,
-              device_id: int) -> Tuple[np.array, np.array]:
+              epoch: int,
+              device_id: int,
+              visualize: bool = True,
+              save_dir: str = 'attention_plots') -> Tuple[np.array, np.array]:
     """
     """
     model.eval()
+    print(f'\n Evaluating on device {device_id} for epoch {epoch}')
 
     num_classes = len(data_dl.dataset.unique_labels)
     y_probs = np.empty((0, num_classes), dtype = np.float32)
     y_true  = np.empty((0, num_classes), dtype = np.float32)
 
     with torch.set_grad_enabled(False):
-        for images,labels in data_dl:
+        for batch_idx, (images,labels) in enumerate(data_dl):
+            if dist.get_rank() == 0:
+                print(f'\n Evaluating batch {batch_idx}')
+
             images = images.cuda(device_id, non_blocking=True)
             labels = labels.cuda(device_id, non_blocking=True)
-            logits, attention_weights = model(images)
-            
+            logits, attention = model(images)
+
             logits = torch.squeeze(logits)
             prob = torch.sigmoid(logits)
             
             y_probs = np.concatenate((y_probs, prob.detach().cpu().numpy()))
             y_true = np.concatenate((y_true, labels.detach().cpu().numpy()))
+
+    if visualize:
+        pred = (prob >= 0.5).float() 
+        visualize_trainable_attention(attention[0], images[0],
+                                        pred[0], num_classes,
+                                        save_plots=True,
+                                        save_dir = f'{cfg.plot_folder}/epoch_{epoch}')
 
     return y_probs, y_true
 
@@ -79,8 +95,9 @@ def train_selfattention(model: ResNetAttention,
                         cfg: Config,
                         train_dl: DataLoader,
                         test_dl: DataLoader,
-                        device_id: int) -> float:
-    """
+                        device_id: int,
+                        visualize: bool = False) -> float:
+    """ 
     """
     optimizer = optim.AdamW(model.parameters(),
                             lr=cfg.optimizer.lr,
@@ -88,9 +105,11 @@ def train_selfattention(model: ResNetAttention,
     classifier_criterion = nn.BCEWithLogitsLoss()
     no_disease_class_idx = train_dl.dataset.unique_labels.index('No Finding')
     num_unique_classes = len(train_dl.dataset.unique_labels)
+    train_loss = 0.0
+    #model, optimizer, start_epoch, _ = load_checkpoint(model, optimizer, "new_16_new_self_attention.pth")
 
     for epoch in range(cfg.train_params.num_epochs):
-        print(f'\nTraining Epoch {epoch} on device {device_id}')
+        print(f'\nTraining Epoch {epoch} on device {device_id}..')
         model.train() 
 
         y_tr_probs = np.empty((0, num_unique_classes),
@@ -98,57 +117,62 @@ def train_selfattention(model: ResNetAttention,
         y_tr_true  = np.empty((0, num_unique_classes),
                                 dtype = np.float32)
 
-        for num_batches,(images, labels) in enumerate(train_dl):
+        for batch_idx,(images, labels) in enumerate(train_dl):
+            if dist.get_rank() == 0:
+                print(f'Training batch {batch_idx}')
 
             optimizer.zero_grad()
             images = images.cuda(device_id, non_blocking=True)
             labels = labels.cuda(device_id, non_blocking=True)
 
-            logits, _ = model(images)
+            logits, attention = model(images)
             prob = torch.sigmoid(logits)
             loss = classifier_criterion(logits, labels) 
+            train_loss += loss.item()
+
             loss.backward()
             optimizer.step()
 
-            print(f'\n Loss on device_id {device_id} for epoch {epoch}: {loss}')
-            
             y_tr_probs = np.concatenate((y_tr_probs, prob.detach().cpu().numpy()))
             y_tr_true = np.concatenate((y_tr_true, labels.detach().cpu().numpy()))
-        
+
+        if dist.get_rank() == 0:
+            print(f'\n Avg. training Loss for epoch {epoch}: \
+                {train_loss/len(train_dl.dataset)}')
+
         y_tr_probs = np.delete(y_tr_probs, no_disease_class_idx, axis=1)
         y_tr_true = np.delete(y_tr_true, no_disease_class_idx, axis=1)
-        
+
         train_auc = get_multiclass_roc_auc_score(y_tr_probs, y_tr_true)
-        if dist.get_rank() == 0:  
-            print(f'Train AUC on device_id {device_id} for epoch {epoch}: {train_auc}')
-        
-        if dist.get_rank() == 0 and (epoch+1)%4 == 0:
+        print(f'\n Train AUC on device_id {device_id} for epoch {epoch}: \
+                    {train_auc}')
+
+        if dist.get_rank() == 0 and (epoch)%4 == 0:
             print(f'Saving checkpoint for epoch {epoch}')
             save_checkpoint(model, optimizer, epoch, loss.item(),
                             file_path=f'{epoch}_{cfg.output_file}')
-        
-        y_te_probs, y_te_true = eval_step(model, test_dl, device_id)
+
+        print(f'Evaluating test set on device {device_id}.. ')
+        y_te_probs, y_te_true = eval_step(model, cfg, test_dl,
+                                          epoch, device_id, visualize=True)
+    
         y_te_probs = np.delete(y_te_probs, no_disease_class_idx, axis=1)
         y_te_true = np.delete(y_te_true, no_disease_class_idx, axis=1)
-
         test_auc = get_dist_auc(y_te_probs, y_te_true)
+
         if dist.get_rank() == 0:
             print(f'Test AUC device_id {device_id} for epoch {epoch}: {test_auc}')
 
-        break
     if dist.get_rank() == 0:
         print('Saving final model')
         save_checkpoint(model, optimizer, epoch, loss.item(),
-                    file_path=cfg.output_file)
-
+                    file_path=cfg.output_file)  
     return test_auc
-
 
 if __name__ == '__main__':
     #Get data config
     config = Config()
 
-    print(type(config))
     #Get Self attention model config
     sa_config = Config(yaml_path = 'configs/selfattention.yaml')
 
@@ -175,8 +199,8 @@ if __name__ == '__main__':
     model = ResNetAttention(tv_models.resnet18(pretrained=True))
     model = model.cuda(device_id)
     model = DDP(model, device_ids=[device_id])
-    test_auc = train_selfattention(model, sa_config, train_dl, test_dl, device_id)
-    print(f"\Test score of Self attention model: {test_auc}")
+    test_auc = train_selfattention(model, sa_config, train_dl, test_dl, device_id, visualize=True)
+    print(f"\n Test score of Self attention model: {test_auc}")
 
     #Clean distributed setup
     cleanup()
--- scripts/train_nih.sh
+++ scripts/train_nih.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
 #SBATCH --job-name=train-nih
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=20
 #SBATCH --gres=gpu:rtx6000:4
-#SBATCH --mem=50G
+#SBATCH --mem=100G
+#SBATCH --qos=normal
 #SBATCH --output=job_%x_%j.out
 #SBATCH --error=job_%x_%j.err
 
 source .venv/bin/activate
-torchrun --standalone --nproc-per-node=4 --nnodes=1 scripts/train_nih.py
+
+srun torchrun --standalone --nproc-per-node=4 --nnodes=1 ../interpretability-bootcamp/reference_implementations/Intepretable-models/Imaging/SelfAttention/train_nih.py
