4e93f92c53dcec62cc0468759e6d5559fbfc7ea5
Ananya Raval <ananya.raval@gmail.com>
2025-03-17 22:35:42 -0400

    Missed commit for train NIH

--- reference_implementations/Intepretable-models/Imaging/SelfAttention/train_nih.py
+++ reference_implementations/Intepretable-models/Imaging/SelfAttention/train_nih.py
@@ -1,8 +1,10 @@
 import os
+import yaml
 import random
 from functools import partial
 
 import torch
+import ultraimport
 import numpy as np
 import pandas as pd
 import torch.nn as nn
@@ -14,24 +16,24 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 import torchvision.models as tv_models
 from typing import Tuple
 
-from data import Config
-from interp.metrics import get_multiclass_roc_auc_score, get_dist_auc
-from interp.utils import *
 from resnetattention import ResNetAttention
-from interp.visualize import visualize_trainable_attention
-from utils import cleanup, worker_init_fn, save_checkpoint, setup_distributed_training, load_checkpoint
-from data import XrayDataset, load_nih_data
 
-def get_data_loaders(cfg: Config,
+ultraimport('__dir__/../utils/metrics.py', ['get_multiclass_roc_auc_score', 'get_dist_auc'], add_to_ns=True)
+ultraimport('__dir__/../utils/model_utils.py', ['load_checkpoint', 'save_checkpoint'], add_to_ns=True)
+ultraimport('__dir__/../utils/visualize.py', ['visualize_trainable_attention'], add_to_ns=True)
+ultraimport('__dir__/../utils/dist_utils.py', ['cleanup', 'worker_init_fn', 'setup_distributed_training'], add_to_ns=True)
+ultraimport('__dir__/../utils/nih_data_utils.py', ['XrayDataset', 'load_nih_data'], add_to_ns=True)
+
+def get_data_loaders(cfg,
                      train_ds: XrayDataset,
                      test_ds: XrayDataset) -> Tuple[DataLoader, DataLoader]:
     """
     """
     init_fn = partial(
         worker_init_fn,
-        num_workers=cfg.train_params.num_workers,
+        num_workers=cfg['train_params']['num_workers'],
         rank=dist.get_rank(),
-        seed=cfg.rseed
+        seed=cfg['rseed']
     )
 
     train_sampler = DistributedSampler(train_ds,
@@ -40,19 +42,19 @@ def get_data_loaders(cfg: Config,
                                       shuffle=True)
     train_dl = DataLoader(train_ds,
                     sampler=train_sampler,
-                    batch_size=cfg.train_params.batch_size,
+                    batch_size=cfg['train_params']['batch_size'],
                     worker_init_fn=init_fn,
                     pin_memory=False,
                     drop_last=True,
                     num_workers=4)
     test_dl = DataLoader(test_ds,
                     sampler=test_sampler,
-                    batch_size=cfg.train_params.batch_size,
+                    batch_size=cfg['train_params']['batch_size'],
                     worker_init_fn=init_fn)
     return train_dl, test_dl
 
 def eval_step(model: ResNetAttention,
-              cfg: Config,
+              cfg,
               data_dl: DataLoader,
               epoch: int,
               device_id: int,
@@ -87,28 +89,33 @@ def eval_step(model: ResNetAttention,
         visualize_trainable_attention(attention[0], images[0],
                                         pred[0], num_classes,
                                         save_plots=True,
-                                        save_dir = f'{cfg.plot_folder}/epoch_{epoch}')
+                                        save_dir = f'{cfg["plot_folder"]}/epoch_{epoch}')
 
     return y_probs, y_true
 
 def train_selfattention(model: ResNetAttention,
-                        cfg: Config,
+                        cfg,
                         train_dl: DataLoader,
                         test_dl: DataLoader,
                         device_id: int,
-                        visualize: bool = False) -> float:
+                        visualize: bool = False,
+                        resume_training: bool = False) -> float:
     """ 
     """
     optimizer = optim.AdamW(model.parameters(),
-                            lr=cfg.optimizer.lr,
-                            weight_decay=float(cfg.train_params.weight_decay))
+                            lr=cfg['optimizer']['lr'],
+                            weight_decay=float(cfg['train_params']['weight_decay']))
     classifier_criterion = nn.BCEWithLogitsLoss()
     no_disease_class_idx = train_dl.dataset.unique_labels.index('No Finding')
     num_unique_classes = len(train_dl.dataset.unique_labels)
-    train_loss = 0.0
-    model, optimizer, start_epoch, _ = load_checkpoint(model, optimizer, "new_8_new_self_attention.pth")
+    train_loss, start_epoch = 0.0, -1
 
-    for epoch in range(start_epoch+1, cfg.train_params.num_epochs):
+    if resume_training:
+        checkpoint_file = f'{cfg["checkpoint_dir"]}/{cfg["checkpoint_file"]}'
+        model, optimizer, start_epoch, _ = load_checkpoint(model, optimizer,
+                                                            checkpoint_file)
+
+    for epoch in range(start_epoch+1, cfg['train_params']['num_epochs']):
         print(f'\nTraining Epoch {epoch} on device {device_id}..')
         model.train() 
 
@@ -149,8 +156,8 @@ def train_selfattention(model: ResNetAttention,
 
         if dist.get_rank() == 0 and (epoch)%4 == 0:
             print(f'Saving checkpoint for epoch {epoch}')
-            save_checkpoint(model, optimizer, epoch, loss.item(),
-                            file_path=f'{epoch}_{cfg.output_file}')
+            checkpoint_file = f'{cfg["checkpoint_dir"]}/{epoch}_{cfg["output_file"]}'
+            save_checkpoint(model, optimizer, epoch, loss.item(), checkpoint_file)
 
         print(f'Evaluating test set on device {device_id}.. ')
         y_te_probs, y_te_true = eval_step(model, cfg, test_dl,
@@ -166,21 +173,23 @@ def train_selfattention(model: ResNetAttention,
     if dist.get_rank() == 0:
         print('Saving final model')
         save_checkpoint(model, optimizer, epoch, loss.item(),
-                    file_path=cfg.output_file)  
+                    f'{cfg["checkpoint_dir"]}/{cfg["output_file"]}')  
     return test_auc
 
 if __name__ == '__main__':
-    #Get data config
-    config = Config()
 
     #Get Self attention model config
-    sa_config = Config(yaml_path = 'configs/selfattention.yaml')
+    yaml_file = os.path.join(os.path.dirname(__file__), 'selfattention.yaml')
+    with open(yaml_file, 'r') as f:
+        sa_config = yaml.safe_load(f)
+    
+    #Get NIH data files
+    train_df, test_df = load_nih_data(data_path = sa_config['data_dir'])
 
-    train_df, test_df = load_nih_data(config)
-
+    print('Loading NIH dataset..')
     #Load NIH dataset, get dataloaders
-    train_ds = XrayDataset(config, train_df)
-    test_ds = XrayDataset(config, test_df)
+    train_ds = XrayDataset(train_df, data_path = sa_config['data_dir'])
+    test_ds = XrayDataset(test_df, data_path = sa_config['data_dir'])
 
     #Distributed setup
     print(f'Setting up distributed training..')
@@ -195,11 +204,11 @@ if __name__ == '__main__':
     print("--------------------------------------------")
 
     #Create Self attention model
-    random_seed(42, True)
+    random.seed(42, True)
     model = ResNetAttention(tv_models.resnet18(pretrained=True))
     model = model.cuda(device_id)
     model = DDP(model, device_ids=[device_id])
-    test_auc = train_selfattention(model, sa_config, train_dl, test_dl, device_id, visualize=True)
+    test_auc = train_selfattention(model, sa_config, train_dl, test_dl, device_id, visualize=True, resume_training=True)
     print(f"\n Test score of Self attention model: {test_auc}")
 
     #Clean distributed setup
