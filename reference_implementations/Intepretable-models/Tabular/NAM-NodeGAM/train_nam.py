ae6a1bb0ce2fb05b769a8e3554f7e924384487e2
Ananya Raval <ananya.raval@gmail.com>
2025-03-19 17:20:16 -0400

    Remove interp package for nam; install kaleido package to view results; move us_130 dataset to Tabular/datasets

--- reference_implementations/Intepretable-models/Tabular/NAM-NodeGAM/train_nam.py
+++ reference_implementations/Intepretable-models/Tabular/NAM-NodeGAM/train_nam.py
@@ -1,24 +1,35 @@
-import argparse
+import os
+import yaml
 from typing import Tuple
 
 import torch
+import ultraimport
 import numpy as np
 import pandas as pd
 from sklearn.metrics import roc_auc_score, f1_score, recall_score, precision_score
 from torch.utils.data import DataLoader, TensorDataset
 
-from data import Config, process_us_130_csv
-from interp.utils import *
-from interp.models import NeuralAdditiveModel, ExULayer, ReLULayer
-from data.data_utils import split_dataset
+nam = ultraimport.create_ns_package('nam', '__dir__/nam')
 
+from nam.model import NeuralAdditiveModel, ExULayer, ReLULayer
+from nam.data_utils import *
+from nam.utils import *
 
 # Some parts were extracted from: https://github.com/kherud/neural-additive-models-pt/tree/master
 
 def get_data_loaders(X: torch.tensor,
                     y: torch.tensor,
                     batch: int) -> Tuple[DataLoader]:
-    """
+    """ Get train, test and validation data loaders
+    Parameters:
+        X: Dataset of features.
+        y: labels.
+        batch: batch size.
+
+    Returns:
+        train_dl: DataLoader for training data.
+        test_dl: DataLoader for testing data.
+        val_dl: DataLoader for validation data.
     """
     train_test_split = split_dataset(X, y,
                                     n_splits=1,
@@ -39,14 +50,26 @@ def get_data_loaders(X: torch.tensor,
 
     return train_dl, test_dl, val_dl
 
-def get_predictions(model, X):
+def get_predictions(model: torch.nn.Module,
+                    X: DataLoader,
+                    device: torch.device) -> Tuple:
+    """ Get predictions from model
+    Parameters:
+        - model: Model to get predictions from.
+        - X: DataLoader with data.
+
+    Returns:
+        x_true: True labels.
+        x_probs: Predicted probabilities.
+        x_preds: Predicted labels.
     """
-    """
+
     model.eval()
     x_true, x_probs = [], []
     with torch.set_grad_enabled(False):
         for _, (features,labels) in enumerate(X):
-            features, labels = features.cuda(), labels.cuda()
+            print('get_predictions: {device}')
+            features, labels = features.to(device), labels.to(device)
             logits, _ = model.forward(features)
             prob = torch.sigmoid(logits)
 
@@ -58,8 +81,23 @@ def get_predictions(model, X):
 
     return x_true, x_probs, x_preds
 
-def train_and_predict(model, cfgs, train, test, val):
-    """
+def train_and_predict(model: torch.nn.Module,
+                      cfgs: dict,
+                      train: DataLoader,
+                      test: DataLoader,
+                      val: DataLoader,
+                      device: torch.device) -> dict:
+    """ Train and predict using NAM model.
+    Parameters:
+        model: NAM model.
+        cfgs: Configuration for training.
+        train: DataLoader for training data.
+        test: DataLoader for testing data.
+        val: DataLoader for validation data.
+        device: Device to use for training.
+    
+    Returns:
+        Dictionary with scores.
     """
     y_tr_true, y_tr_probs, y_tr_preds = [], [], []
     y_te_true, y_te_probs, y_tr_preds = [], [], []
@@ -67,15 +105,17 @@ def train_and_predict(model, cfgs, train, test, val):
     auc, f1, precision, recall = [], [], [], []
 
     classifier_criterion = torch.nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=cfgs.optimizer.lr)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, gamma=0.995, step_size=1) #Add step size here
+    optimizer = torch.optim.AdamW(model.parameters(), lr=cfgs['optimizer']['lr'])
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                cfgs['scheduler']['step_size'],
+                                                cfgs['scheduler']['step_size'])
     optimizer.zero_grad()
 
-    for epoch in range(cfgs.train_params.epochs):
+    for epoch in range(cfgs['train_params']['epochs']):
         model.train()
 
         for _,(features,labels) in enumerate(train): 
-            features, labels= features.cuda(),labels.cuda()
+            features, labels= features.to(device),labels.to(device)
             logits, _ = model.forward(features)
             prob = torch.sigmoid(logits)
             loss = classifier_criterion(logits, labels)
@@ -88,8 +128,8 @@ def train_and_predict(model, cfgs, train, test, val):
             y_tr_preds.extend(prob.tolist())
         
         scheduler.step()
-        y_val_true, y_val_probs, y_val_preds = get_predictions(model, val)
-        y_te_true, y_te_probs, y_te_preds = get_predictions(model, test)
+        y_val_true, y_val_probs, y_val_preds = get_predictions(model, val, device)
+        y_te_true, y_te_probs, y_te_preds = get_predictions(model, test, device)
 
         val_auc = roc_auc_score(y_val_true, y_val_probs)
         train_auc = roc_auc_score(y_tr_true, y_tr_preds)
@@ -107,15 +147,15 @@ def train_and_predict(model, cfgs, train, test, val):
             'precision': (np.mean(precision),np.std(precision)),
             'recall': (np.mean(recall),np.std(recall))}
 
-if __name__ == "__main__":
+def main():
+    """ Main function to train NAM model on US_130 dataset.
+    """
+    #Get nam model config
+    yaml_file = os.path.join(os.path.dirname(__file__), 'nam.yaml')
+    with open(yaml_file, 'r') as f:
+        config = yaml.safe_load(f)
 
-    #Get data config
-    config = Config()
-    data_file = config.get_datafiles('us_130')['diabetic_data.csv']
-    
-    #Get NAM model config
-    nam_config = Config(yaml_path = 'configs/nam.yaml')
-
+    data_file = os.path.join(config['data_dir'], config['data_file'])
     data = pd.read_csv(data_file)
     df = process_us_130_csv(data)
 
@@ -124,8 +164,7 @@ if __name__ == "__main__":
     X, y = torch.tensor(X.values, dtype=torch.float32), torch.tensor(y.values, dtype=torch.float32)
 
     #Get train test val split
-    train_dl, test_dl, val_dl = get_data_loaders(X, y, nam_config.train_params.batch_size)
-
+    train_dl, test_dl, val_dl = get_data_loaders(X, y, config['train_params']['batch_size'])
 
     print("------------------------------")
     print("Training NAM on US_130 dataset")
@@ -137,20 +176,20 @@ if __name__ == "__main__":
     random_seed(42, True)
 
     shallow_units = calculate_n_units(get_full_data(train_dl),
-                                      nam_config.nam.n_basis_functions,
-                                      nam_config.nam.units_multiplier)
+                                      config['nam']['n_basis_functions'],
+                                      config['nam']['units_multiplier'])
     
-    shallow_layer = ExULayer if nam_config.nam.shallow_layer == "exu" else ReLULayer
-    hidden_layer = ExULayer if nam_config.nam.hidden_layer == "exu" else ReLULayer
+    shallow_layer = ExULayer if config['nam']['shallow_layer'] == "exu" else ReLULayer
+    hidden_layer = ExULayer if config['nam']['hidden_layer'] == "exu" else ReLULayer
 
     model = NeuralAdditiveModel(input_size=X.shape[-1],
                                 shallow_units=shallow_units,
                                 shallow_layer=shallow_layer,
                                 hidden_layer=hidden_layer,
-                                hidden_dropout=nam_config.nam.dropout,
-                                feature_dropout=nam_config.nam.feature_dropout)
-    model = model.cuda()   
-    nam_scores = train_and_predict(model, nam_config, train_dl, test_dl, val_dl)
+                                hidden_dropout=config['nam']['dropout'],
+                                feature_dropout=config['nam']['feature_dropout'])
+    model = model.to(device)  
+    nam_scores = train_and_predict(model, config, train_dl, test_dl, val_dl, device)
 
     print(f"\nScores of NAM model:")
     print(f"F1: Mean: {nam_scores['f1'][0]} \t  \
@@ -161,3 +200,7 @@ if __name__ == "__main__":
           Std. Deviation: {nam_scores['precision'][1]}")
     print(f"Recall: Mean: {nam_scores['recall'][0]} \t \
           Std. Deviation: {nam_scores['recall'][1]}")
+
+
+if __name__ == "__main__":
+    main()
